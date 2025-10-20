04b0b4c90e8ce2bbbd09c4cba935ce4fc99464fe
azapokou-art <azapokou@gmail.com>
2025-10-20 17:26:26 -0300

    "feat: implementa sistema de segurança&paginação

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { cadastrarUsuario, loginUsuario, refreshTokenHandler } = require('../controllers/usuarioController');
+const { cadastrarUsuario, loginUsuario, refreshTokenHandler, logout, } = require('../controllers/usuarioController');
+const { autenticarToken } = require('../middleware/authMiddleware');
 ``
 router.post('/usuarios',cadastrarUsuario);
 router.post('/login', loginUsuario);
 router.post('/refresh-token', refreshTokenHandler);
+router.post('/logout', autenticarToken, logout);
 module.exports = router;
